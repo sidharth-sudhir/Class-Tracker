b0a759dbd8d0af16431056034178ecfd498d537f
SSsudhir <sidharth2ndacc@gmail.com>
2021-09-06 18:55:03 -0700

    Complete functionality to add new grades

--- Class Tracker/Controllers/GradeListViewController.swift
+++ Class Tracker/Controllers/GradeListViewController.swift
@@ -8,7 +8,23 @@
 import UIKit
 import RealmSwift
 
-class GradeListViewController: SwipeTableViewController {
+protocol HomeViewController : AnyObject {
+    func comingHome()
+}
+
+protocol DismissingViewController : AnyObject {
+    var home : HomeViewController? {get set}
+}
+
+extension DismissingViewController where Self : UIViewController {
+    func notifyComingHome() {
+        if self.isBeingDismissed || self.isMovingFromParent {
+            self.home?.comingHome()
+        }
+    }
+}
+
+class GradeListViewController: SwipeTableViewController, HomeViewController {
     var grades: Results<Grade>?
     let realm = try! Realm()
     
@@ -35,6 +51,10 @@ class GradeListViewController: SwipeTableViewController {
         
         if let grade = grades?[indexPath.row] {
             cell.textLabel?.text = grade.title
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 25.0)
+            cell.detailTextLabel?.text = String(format: "%.1f", grade.percentage) + "%"
+            cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 15.0)
+            cell.detailTextLabel?.textColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         }
         
         return cell
@@ -57,4 +77,19 @@ class GradeListViewController: SwipeTableViewController {
             }
         }
     }
+    
+    @IBAction func addNewGradePressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToCreateGrade", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! NewGradeViewController
+        destinationVC.selectedSyllabus = selectedSyllabus
+        destinationVC.home = self
+    }
+    
+    func comingHome() {
+        tableView.reloadData()
+    }
+    
 }
--- Class Tracker/Controllers/NewGradeViewController.swift
+++ Class Tracker/Controllers/NewGradeViewController.swift
@@ -8,6 +8,57 @@
 import UIKit
 import RealmSwift
 
-class NewGradeViewController: UIViewController {
-
+class NewGradeViewController: UIViewController, DismissingViewController {
+    var home: HomeViewController?
+    
+    var selectedSyllabus: Syllabus?
+    let realm = try! Realm()
+    
+    @IBOutlet weak var gradeName: UITextField!
+    @IBOutlet weak var gradeRecieved: UITextField!
+    @IBOutlet weak var gradeOutOf: UITextField!
+    @IBOutlet weak var gradePercentage: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        saveButton.clipsToBounds = true
+        saveButton.layer.cornerRadius = 10.0
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.notifyComingHome()
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        save()
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func save() {
+        if let currentSyllabus = self.selectedSyllabus {
+            do {
+                try realm.write {
+                    let newGrade = Grade()
+                    newGrade.title = gradeName.text!
+                    
+                    if gradeRecieved.text?.count == 0 || gradeOutOf.text?.count == 0 {
+                        newGrade.percentage = Float(gradePercentage.text!)!
+                    } else {
+                        newGrade.percentage = (Float(gradeRecieved.text!)! / Float(gradeOutOf.text!)!) * 100
+                    }
+                    
+                    currentSyllabus.grades.append(newGrade)
+                }
+            } catch {
+                print("Error saving grade: \(error)")
+            }
+        }
+    }
+    
 }
+
+
